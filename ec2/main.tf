b5633707efd9fcced35a917d9dc78adc947204bb
Jyotshna Sharma <70006520+sharmajs17@users.noreply.github.com>
2023-04-12 17:17:47 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -8,7 +8,7 @@ resource "tls_private_key" "key" {
 resource "aws_key_pair" "key_pair" {
   key_name   = "key"
   public_key = tls_private_key.key.public_key_openssh
-
+}
 #  provisioner "local-exec" {
 #    command = "echo '${tls_private_key.key.private_key_pem}' > ./key.pem"
 #  }
